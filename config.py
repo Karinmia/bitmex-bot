adc522ae5a205e8fd1ee665aed1e6b28e86be832
Karyna Avramenko <karinmia99avr@gmail.com>
2019-04-20 17:26:31 +0300

    Changed .gitignore

---
+++ config.py
@@ -0,0 +1,8 @@
+BOT_TOKEN = ''
+
+DATABASE_NAME = ''
+DATABASE_LOGIN = ''
+DATABASE_PASSWORD = ''
+
+# Create local_config.py and configure all the required settings for bot (listed above)
+from local_config import *
